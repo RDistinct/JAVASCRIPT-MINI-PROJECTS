85f84b2267fae28779347577cd25fcfd97988f5f
ReubenDev <reubenkiarie8m@gmail.com>
2023-02-25 19:35:34 +0300

    add TABS explanation on element id

--- 11. Tabs/app.js
+++ 11. Tabs/app.js
@@ -21,7 +21,10 @@ about.addEventListener("click", function (event) {
       //access each article using the article parameter
       article.classList.remove("active");
     });
-    //find the article element that has same id. How it does this I DONT KNOW???!!!
+    //find the article element that has same id. How it does this, I DONT KNOW???!!!
+
+    //25th Feb 2023 after NodeSchool session an idea comes to my mind & i figure out WHY!!!
+    /* The id we are getting on line 12 is either history, vision or goals for the button. We then create an element that has the SAME id we got above. Thats just it. Get an id on line 12 then create an element with the same id. PERIOD! Simple!  */
     const element = document.getElementById(id);
     element.classList.add("active");
   }
