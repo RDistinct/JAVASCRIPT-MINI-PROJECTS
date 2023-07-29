dc2840fdd2bc39702b98614dec0c25347611d476
ReubenDev <reubenkiarie8m@gmail.com>
2023-07-29 10:28:18 +0300

    add COMPLETED GALLERY OOP PROJECT

--- OOP Projects/Photo Gallery/app.js
+++ OOP Projects/Photo Gallery/app.js
@@ -32,11 +32,12 @@ function Gallery(element) {
   this.closeBtn = getElement(".close-btn");
   this.prevBtn = getElement(".prev-btn");
   this.nextBtn = getElement(".next-btn");
+
   //BIND MODAL BUTTONS TO THE GALLERY
-  
   this.closeModal = this.closeModal.bind(this)
   this.nextImage = this.nextImage.bind(this)
   this.prevImage = this.prevImage.bind(this)
+  this.chooseImage = this.chooseImage.bind(this)
 
 
 
@@ -63,6 +64,10 @@ Gallery.prototype.openModal = function (selectedImage,list) {
     return `<img src="${image.src}" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id?"modal-img selected":"modal-img"}"/>`
   }).join('') 
   this.modal.classList.add("open");
+  this.closeBtn.addEventListener("click", this.closeModal);
+  this.nextBtn.addEventListener("click", this.nextImage);
+  this.prevBtn.addEventListener("click", this.prevImage);
+  this.modalImages.addEventListener('click', this.chooseImage);
 };
 
 Gallery.prototype.setMainImage = function(selectedImage){
@@ -77,14 +82,29 @@ Gallery.prototype.closeModal = function(){
   this.closeBtn.removeEventListener('click', this.closeModal)
   this.nextBtn.removeEventListener('click', this.nextImage)
   this.prevBtn.removeEventListener('click', this.prevImage)
+  this.modalImages.removeEventListener('click', this.chooseImage)
 }
 
 Gallery.prototype.nextImage = function(){
-
+const selected = this.modalImages.querySelector('.selected');
+const next = selected.nextElementSibling || this.modalImages.firstElementChild
+selected.classList.remove('selected');
+next.classList.add('selected')
+this.setMainImage(next)
 }
 
 Gallery.prototype.prevImage = function(){
+  const selected = this.modalImages.querySelector('.selected');
+  const prev = selected.previousElementSibling || this.modalImages.lastElementChild
+  selected.classList.remove('selected');
+  prev.classList.add('selected')
+  this.setMainImage(prev)
+}
 
+Gallery.prototype.chooseImage = function(e){
+  if(e.target.classList.contains('modal-img')){
+    console.log(e.target);
+  }
 }
 
 
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # THIS IS THE PROJECT SECTION OF JS TUTORIAL & PROJECTS(EXTENSION)#
 
-### SHORTCUT FOR MOVING THE CURSOR AT THE START/END OF LINE:
 
-    End button -> move to end of line
-    Home button -> move to start of line
     To REFRESH/RELOAD vs-code -> Ctrl + Shift + P then type Reload window
-    Find something within a file -> Ctrl + F
+
+The Gallery Image Selector in OOP was a bit challenging due to the bindings that had to be done.    
